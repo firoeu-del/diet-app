e7bef8fd4e354442833130a3b5499e5049dec866
firoeu-del <firoeu@gmail.com>
2026-07-06 04:11:24 +0330

    تغییر نام به Dietrix، فونت IRANSansDN، رژیم افزایش وزن و فیلتر بخش‌های نتایج

--- sw.js
+++ sw.js
@@ -1,20 +1,26 @@
-const CACHE = "diet-app-v3";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+const CACHE = "dietrix-v4";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./font.css", "./icon.svg"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
-self.addEventListener("fetch", e => {
-  if (e.request.method !== "GET") return;
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-      if (resp.ok) {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-      }
-      return resp;
-    }).catch(() => caches.match("./index.html")))
+    caches.match(e.request).then((r) =>
+      r ||
+      fetch(e.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"))
+    )
   );
 });
